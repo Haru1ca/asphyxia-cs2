3cbc5a8b2042c4398b73e4f31ef5a345bf96dec1
Haru1ca <68890937+Haru1ca@users.noreply.github.com>
2024-08-30 19:26:01 +0800

    Update painter.cpp

--- cstrike/features/legitbot/painter.cpp
+++ cstrike/features/legitbot/painter.cpp
@@ -1,6 +1,6 @@
 #include "painter.h"
-#include "core/sdk.h"
-#include "sdk/interfaces/iengineclient.h"
+#include "../../core/sdk.h"
+#include "../../sdk/interfaces/iengineclient.h"
 Painter::Painter()
 {
 }
